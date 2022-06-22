f6255bfbf3f33ef1bb9532fc042e3540d5e681b0
Bair Arshaev <ars-bair@yandex.ru>
2022-06-22 21:49:26 +0300

    Edit code 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.mult("101", "111"));
+        System.out.println(binOps.sum("111", "1101"));
+    }
+}
